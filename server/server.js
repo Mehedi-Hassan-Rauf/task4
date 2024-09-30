de330b64ad62638ac6232bcc4bb32695ec09cfa9
Rauf <raufnx@gmail.com>
2024-09-30 17:44:24 +0600

    cors

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const dotenv = require("dotenv");
-const path = require("path");
 
 // Initialize Express App
 dotenv.config();
@@ -14,12 +13,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 // Use CORS middleware before your routes
-app.use(cors({
-    origin: "https://task4-nu-ashy.vercel.app", // Adjust this to your frontend URL
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    credentials: true, // Set to true if you need to include cookies or authorization headers
-    allowedHeaders: ["Origin","Content-Type","Accept","Authorization","X-Request-With",], 
-}));
+app.use(cors());
 
 app.use(bodyParser.json());
 
@@ -52,7 +46,7 @@ const authenticateToken = (req, res, next) => {
     });
 };
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
     res.json({ message: 'Hello World' });
 });
 
