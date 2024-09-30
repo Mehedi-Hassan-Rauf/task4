e9f0d8ed67f0ddca0ed651971c4be9334849caee
Rauf <raufnx@gmail.com>
2024-09-30 16:13:22 +0600

    url change

--- server/index.js
+++ server/index.js
@@ -14,12 +14,13 @@ const port = process.env.PORT || 5000;
 
 
 // Middleware
-app.use(cors({
-    origin: "https://task4-nu-ashy.vercel.app/",
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    credentials: true, 
-    allowedHeaders: ["Content-Type", "Authorization"],
-}));
+// app.use(cors({
+//     origin: "https://task4-nu-ashy.vercel.app/",
+//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+//     credentials: true, 
+//     allowedHeaders: ["Content-Type", "Authorization"],
+// }));
+app.use(cors());
 app.use(bodyParser.json());
 
 // Database Connection
