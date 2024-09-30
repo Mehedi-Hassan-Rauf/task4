e2bc55a750ad86cc48568d3f646eaf5bf8aca5a1
Rauf <raufnx@gmail.com>
2024-09-30 15:36:30 +0600

    cors

--- server/index.js
+++ server/index.js
@@ -15,10 +15,12 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: ["https://task4-git-master-raufs-projects-88329df5.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    credentials: true,
-  },));
+    origin: "https://task4-git-master-raufs-projects-88329df5.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    credentials: true, 
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
+app.options('*', cors());
 app.use(bodyParser.json());
 
 // Database Connection
